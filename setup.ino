955cf085ecb7f0298c7046294431db5897bad07e
Daniel <visual.eyes.success@gmail.com>
2023-07-09 13:55:59 +0200

    Update setup.ino

--- setup.ino
+++ setup.ino
@@ -21,35 +21,3 @@
 unsigned long lastLoggingTime = 0;
 LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLS, LCD_ROWS);
 File dataFile;
-
-void setup() {
-  // Initialize LCD screen
-  lcd.begin(LCD_COLS, LCD_ROWS);
-  lcd.print("Decibel Level:");
-
-  // Initialize data logging shield
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // Wait for serial connection
-  }
-
-  if (!SD.begin(SD_CHIP_SELECT)) {
-    lcd.clear();
-    lcd.print("Error: SD card");
-    while (true) {
-      ; // Handle SD card error
-    }
-  }
-
-  dataFile = SD.open("datalog.txt", FILE_WRITE);
-  if (!dataFile) {
-    lcd.clear();
-    lcd.print("Error: Data file");
-    while (true) {
-      ; // Handle data file error
-    }
-  }
-
-  // Initialize sound sensor
-  pinMode(SOUND_SENSOR_PIN, INPUT);
-}
